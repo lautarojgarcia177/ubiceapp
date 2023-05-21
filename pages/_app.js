0ad4da1550c02db7fd7406cc32d069969483fedb
Lautaro Garcia <lautarojgarcia177@gmail.com>
2023-05-21 13:33:57 -0300

    add styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
-import "@/styles/globals.css";
-import { Amplify } from "aws-amplify";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "@aws-amplify/ui-react/styles.css";
+import "@/styles/custom-theme.css"
+import "@/styles/styles.css"
+
+import { Amplify } from "aws-amplify";
 import awsExports from "../aws-exports.js";
 import { Montserrat } from "next/font/google";
 import Layout from "@/components/Layout";
